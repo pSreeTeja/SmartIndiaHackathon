c223bfeba314527a628ec0a0e1c7e523a93bb04f
NK Pranesh <nkpranesh@gmail.com>
2022-03-18 18:43:12 +0530

    landing Page done

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/navbar.css";
 
 class Navbar extends Component {
-  state = {};
+  state = {
+    loginActive: false,
+    signupActive: true,
+  };
   render() {
-    return <div>Navbar</div>;
+    return (
+      <div className="NMainDiv">
+        <div className="NLogoDiv">
+          <span className="NLogo">
+            <span className="NX">X</span>plore
+          </span>
+        </div>
+        <div className="NButtonsDiv">
+          <div
+            className={
+              this.state.signupActive
+                ? "NSignupButton NButton Active"
+                : "NSignupButton NButton"
+            }
+          >
+            <Link to="/signup"> Sign Up</Link>
+          </div>
+          <div
+            className={
+              this.state.loginActive
+                ? "NLoginButton NButton Active"
+                : "NLoginButton NButton"
+            }
+          >
+            <Link to="/login"> Login</Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
