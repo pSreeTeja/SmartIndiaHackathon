83a456fd693ba102f1373ead135bf4d4fa0359de
NK Pranesh <nkpranesh@gmail.com>
2022-03-18 09:07:26 +0530

    React

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./Containers/LandingPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
